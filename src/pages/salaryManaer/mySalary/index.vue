816ad2957ca11e9172f6e64b44332496e634a7ea
youranxin <470635910@qq.com>
2018-06-29 19:45:49 +0800

    我的绩效

--- src/pages/salaryManaer/mySalary/index.vue
+++ src/pages/salaryManaer/mySalary/index.vue
@@ -1,11 +1,144 @@
 <template>
+    <div id="assetslocation">
+        <Card>
 
+        <Form ref="searchData" :model="addDepForm" inline :label-width="60">
+            <FormItem label="月份">
+                <DatePicker type="month"
+                            placeholder="月份"
+                            @on-change="_addDepMonthChange"
+                            :value="addDepForm.month"></DatePicker>
+            </FormItem>
+
+        </Form>
+        <Table :loading="tableLoading"
+               :height="tableHeight"
+               :columns="header.columns"
+               :data="tableData.data"
+        ></Table>
+        </Card>
+
+
+    </div>
 </template>
 
 <script>
+    import moment from 'moment';
+    import pageMixin from '@/mixins/pageMixin';
+
+    import 'vue-easytable/libs/themes-base/index.css';
+    import {VTable, VPagination} from 'vue-easytable';
+    import fsTablePage from '@/baseComponents/fs-table-page';
+    const NOW_MONTH = moment().format('YYYY-MM');
+
     export default {
-        name: 'mySalary'
+        name: 'mySalary',
+        components: {
+            VTable, VPagination, fsTablePage
+        },
+        data() {
+            return {
+                tableHeight: 720,
+                filterOpt: {
+                    name: ''
+                },
+                addDepForm: {
+                    month: NOW_MONTH,
+                    organizeName: ''
+                },
+                positionList: [],
+                baseInfo: {
+                    name: '',
+                    remarks: ''
+                },
+                rowColumn: {},
+                header: {
+                    id: '',
+                    columns: [],
+                    name: ''
+                },
+                tableData: {
+                    data: []
+                },
+                changeInfoModal: false
+
+            };
+        },
+
+        mixins: [pageMixin],
+        created () {
+            this.getPositionList();
+
+        },
+        methods: {
+
+
+            getPositionList() {
+                let data = {};
+                data.time = this.addDepForm.month;
+                this.$http.post('/perform/getMyAllList', data).then((res) => {
+                    if (res.success) {
+                        this.detail(res.data);
+
+                    }
+                });
+            },
+            _addDepMonthChange(date) {
+                this.addDepForm.month = date;
+                this._getAttendanceData();
+            },
+            _getAttendanceData() {
+                let data = {};
+                data.time = this.addDepForm.month;
+                this.getList('/perform/getMyAllList', data);
+            },
+            detail(data) {
+
+                this.tableData.data = [];
+                var columnObj = {};
+                var tableData = {};
+                var tableData1 = {};
+                data.forEach((item, index) => {
+                    console.log(item)
+                    let params = item;
+                try {
+                    columnObj = eval('(' + params.kv + ')');
+                    tableData = eval('(' + params.values + ')');
+
+                    tableData.score = item.score;
+                } catch (e) {
+                    this.$Message.error('数据格式不规范');
+                }
+                this.header.id = params.id;
+                this.header.name = params.name;
+                // this.tableData.key_id = params.id;
+                    var ab= '';
+                for (let key in columnObj) {
+                    this.header.columns.push({
+                        'title': columnObj[key],
+                        'key': key,
+                        'align': 'center'
+                    });
+                    ab = key;
+
+                }
+                    this.header.columns.push({
+                        'title': '得分',
+                        'key': 'score',
+                        'align': 'center'
+                    });
+                if (tableData === null) {
+                    tableData = [];
+                }
+                })
+
+
+                this.tableData.data.push(tableData);
+            }
+
+        }
     };
+
 </script>
 
 <style scoped>
