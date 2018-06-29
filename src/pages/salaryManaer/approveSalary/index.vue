17919cd2b621052912ce0ccbe2343785bdc5794a
hack_uk@163.com <zh.rui33cd>
2018-06-29 09:37:40 +0800

    规章制度

---
+++ src/pages/salaryManaer/approveSalary/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="approveSalary">
+        <Card>
+            
+        </Card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'approveSalary'
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -538,6 +538,29 @@ export const appRouter = [
         ]
     },
     {
+        path: '/salaryManaer',
+        icon: 'ios-star',
+        name: 'salaryManaer',
+        title: '绩效管理',
+        component: Main,
+        children: [
+            {
+                path: 'newSalary',
+                icon: 'ios-star',
+                name: 'newSalary',
+                title: '绩效方案管理',
+                component: _import('salaryManaer/newSalary')
+            },
+            {
+                path: 'approveSalary',
+                icon: 'ios-star',
+                name: 'approveSalary',
+                title: '绩效打分',
+                component: _import('salaryManaer/approveSalary')
+            }
+        ]
+    },
+    {
         path: '/trainManage',
         icon: 'ribbon-a',
         name: 'trainManage',
