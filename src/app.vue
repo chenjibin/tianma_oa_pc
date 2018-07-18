e107d7d3cbdc3f56ac74c6cee8eee5b4a234ff8d
Francis <363840817@qq.com>
2018-07-18 18:04:21 +0800

    日志指导回复

--- src/app.vue
+++ src/app.vue
@@ -8,88 +8,105 @@
 
 <script>
     export default {
-        data () {
+        data() {
             return {
                 theme: this.$store.state.app.themeColor
             };
         },
-        mounted () {
+        mounted() {
 
         },
-        beforeDestroy () {
+        beforeDestroy() {
 
         },
         created() {
 
         },
-        methods: {
-
-        }
+        methods: {}
     };
 </script>
 <style lang="less">
-  html,body{
-    width: 100%;
-    height: 100%;
-    background: #f0f0f0;
-    overflow: hidden;
-  }
-  ul {
-    list-style: none;
-  }
-  .gallery-background[lazy=loading] {
-      background-color: #eee;
-  }
-  .fade-page-enter-active, .fade-page-leave-active {
-      transition: opacity .5s;
-  }
-  .fade-page-enter, .fade-page-leave-to /* .fade-leave-active below version 2.1.8 */ {
-      opacity: 0;
-  }
-  .ivu-scroll-container {
-    overflow-y: auto;
-  }
-  .ivu-select-dropdown {
-    max-height: 300px;
-  }
-  .ivu-btn-text:focus {
-    box-shadow: none;
-  }
-  .fs-square-img {
-    position: relative;
-    padding-top: 100%;
-    width: 100%;
-  }
-  .fs-square-img img {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-  .flex-center {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-  .app-main{
-    width: 100%;
-    height: 100%;
-  }
-  .no-result-block {
-    margin-top: 88px;
-    padding-bottom: 88px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    img {
-      width: 300px;
+    html, body {
+        width: 100%;
+        height: 100%;
+        background: #f0f0f0;
+        overflow: hidden;
     }
-    .info {
-      font-size: 18px;
-      font-weight: 700;
+
+    ul {
+        list-style: none;
     }
-  }
+
+    .gallery-background[lazy=loading] {
+        background-color: #eee;
+    }
+
+    .fade-page-enter-active, .fade-page-leave-active {
+        transition: opacity .5s;
+    }
+
+    .fade-page-enter, .fade-page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+    .down-enter-active, .down-leave-active {
+        transition: transform .5s;
+    }
+
+    .down-enter, .down-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        transform: translate3d(0, -100%, 0);
+    }
+
+    .ivu-scroll-container {
+        overflow-y: auto;
+    }
+
+    .ivu-select-dropdown {
+        max-height: 300px;
+    }
+
+    .ivu-btn-text:focus {
+        box-shadow: none;
+    }
+
+    .fs-square-img {
+        position: relative;
+        padding-top: 100%;
+        width: 100%;
+    }
+
+    .fs-square-img img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+    }
+
+    .flex-center {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .app-main {
+        width: 100%;
+        height: 100%;
+    }
+
+    .no-result-block {
+        margin-top: 88px;
+        padding-bottom: 88px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        img {
+            width: 300px;
+        }
+        .info {
+            font-size: 18px;
+            font-weight: 700;
+        }
+    }
 </style>
