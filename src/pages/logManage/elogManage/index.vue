0adc1fe6fb4ec60509847d0fb5c70311b462eb62
xiaoyan666 <598763741@163.com>
2018-08-24 11:59:04 +0800

    添加一个刷新

--- src/pages/logManage/elogManage/index.vue
+++ src/pages/logManage/elogManage/index.vue
@@ -499,6 +499,7 @@
                             if (res.success) {
                                 this.$Message.success('评价成功!');
                                 this.$refs.fsTable.getListData();
+                                this._getNoWritePeoloe();
                                 this.checkLogFlag = false;
                             }
                         });
